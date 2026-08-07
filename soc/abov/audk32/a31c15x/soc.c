a353992df6f529956d04e46dd7c910c142d07825
Hyunseok Choi(최현석) <hyunseok.choi@abov.co.kr>
2026-08-07 19:41:48 +0900

    Added A31C156 define to build starter-kit target.

--- soc/abov/audk32/a31c15x/soc.c
+++ soc/abov/audk32/a31c15x/soc.c
@@ -13,12 +13,20 @@
 #include <zephyr/init.h>
 #include <cmsis_core.h>
 
+#include "abov_config.h"
+
 /**
  * @brief Perform basic hardware initialization at boot.
  *
  * This needs to be run from the very beginning.
+ *
+ * Deliberately calls PRV_CHIPSET_Init() (WDT disable, SCU register-write
+ * enable, flash wait-state) instead of the vendor's SystemInit() from
+ * system_a31xxxx.c: that function also relocates VTOR to a vendor
+ * __VECTOR_TABLE that doesn't exist in a Zephyr build, since Zephyr owns the
+ * vector table and VTOR itself.
  */
 void soc_early_init_hook(void)
 {
-        SystemInit();
+        PRV_CHIPSET_Init();
 }
